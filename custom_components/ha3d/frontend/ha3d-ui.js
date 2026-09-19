f9068f0da85f56de0b7c2da9f36bd5f6dde21c2c
Jackson Gomes <97919205+Jackson-Gomes@users.noreply.github.com>
2026-09-19 18:23:19 -0300

    fix: install cinematic controls for already-mounted HA3D panel

---
+++ custom_components/ha3d/frontend/ha3d-ui.js
@@ -0,0 +1,29 @@
+import "./ha3d-cinematic.js?v=0.1.6";
+
+const Panel = customElements.get("ha3d-panel");
+if (!Panel) throw new Error("HA3D panel was not registered");
+
+const proto = Panel.prototype;
+
+if (!proto.__ha3dCinematicUiLateInstall) {
+  proto.__ha3dCinematicUiLateInstall = true;
+
+  const hassDescriptor = Object.getOwnPropertyDescriptor(proto, "hass");
+  if (hassDescriptor?.set) {
+    Object.defineProperty(proto, "hass", {
+      configurable: true,
+      enumerable: hassDescriptor.enumerable,
+      get: hassDescriptor.get,
+      set(value) {
+        hassDescriptor.set.call(this, value);
+        queueMicrotask(() => this._installCinematicControls?.());
+      },
+    });
+  }
+
+  const originalConnectedCallback = proto.connectedCallback;
+  proto.connectedCallback = function () {
+    originalConnectedCallback.call(this);
+    queueMicrotask(() => this._installCinematicControls?.());
+  };
+}
