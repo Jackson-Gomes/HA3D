173dbf549f8368d7f1dab07ba5d801edb06cb31e
Jackson Gomes <97919205+Jackson-Gomes@users.noreply.github.com>
2026-09-20 03:48:09 -0300

    Bump cinematic light delay module cache token

--- custom_components/ha3d/frontend/ha3d-entry.js
+++ custom_components/ha3d/frontend/ha3d-entry.js
@@ -14,4 +14,4 @@ import "./ha3d-marker-filter-bar.js?v=marker-filter-3";
 import "./ha3d-views-icon.js?v=views-icon-2";
 import "./ha3d-scene-prefix-menu.js?v=scene-prefix-menu-1";
 import "./ha3d-xray-idle-v4.js?v=xray-idle-4";
-import "./ha3d-cinematic-light-delay.js?v=cinematic-light-delay-2";
+import "./ha3d-cinematic-light-delay.js?v=cinematic-light-delay-3";
