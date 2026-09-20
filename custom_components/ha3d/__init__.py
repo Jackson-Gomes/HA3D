48b1d4530c400734d6115ebaa9c82c9252b8f5e8
Jackson Gomes <97919205+Jackson-Gomes@users.noreply.github.com>
2026-09-20 17:33:11 -0300

    Version HA3D frontend for X-Ray open sensor alerts

--- custom_components/ha3d/__init__.py
+++ custom_components/ha3d/__init__.py
@@ -21,7 +21,7 @@ from .http import HA3DConfigView, HA3DModelUploadView
 from .storage import HA3DStore
 
 
-FRONTEND_VERSION = "0.1.10"
+FRONTEND_VERSION = "0.1.11"
 
 
 async def async_setup(hass: HomeAssistant, config: ConfigType) -> bool:
