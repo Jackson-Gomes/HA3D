26bca247a8a12ee3265d004faff155f76e8643b1
Jackson Gomes <97919205+Jackson-Gomes@users.noreply.github.com>
2026-09-20 03:22:32 -0300

    Load X-Ray idle watchdog v3

--- custom_components/ha3d/frontend/ha3d-entry.js
+++ custom_components/ha3d/frontend/ha3d-entry.js
@@ -13,4 +13,4 @@ import "./ha3d-cinematic-off-v2.js?v=cinematic-off-2";
 import "./ha3d-marker-filter-bar.js?v=marker-filter-3";
 import "./ha3d-views-icon.js?v=views-icon-2";
 import "./ha3d-scene-prefix-menu.js?v=scene-prefix-menu-1";
-import "./ha3d-xray-theme.js?v=xray-idle-2";
+import "./ha3d-xray-idle-v3.js?v=xray-idle-3";
