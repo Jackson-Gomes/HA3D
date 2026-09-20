bd8572eb4d1d01e24520048660bdcf90c63d659c
Jackson Gomes <97919205+Jackson-Gomes@users.noreply.github.com>
2026-09-20 01:56:37 -0300

    Bust cache for bottom marker menu fix

--- custom_components/ha3d/frontend/ha3d-entry.js
+++ custom_components/ha3d/frontend/ha3d-entry.js
@@ -10,4 +10,4 @@ import "./ha3d-scene-icon.js?v=scene-icon-1";
 import "./ha3d-3d-printer-icon.js?v=3d-printer-icon-1";
 import "./ha3d-window-icons.js?v=window-icons-1";
 import "./ha3d-cinematic-off-v2.js?v=cinematic-off-2";
-import "./ha3d-marker-filter-bar.js?v=marker-filter-1";
+import "./ha3d-marker-filter-bar.js?v=marker-filter-2";
