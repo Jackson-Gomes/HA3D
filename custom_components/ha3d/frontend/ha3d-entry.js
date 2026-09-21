2fef0248d18502d0a087c9ad8f514384c311d745
Jackson Gomes <gomes_jackson@outlook.com>
2026-09-21 09:50:15 -0300

    feat: add experimental 3D editor and advanced markers

--- custom_components/ha3d/frontend/ha3d-entry.js
+++ custom_components/ha3d/frontend/ha3d-entry.js
@@ -21,3 +21,4 @@ import "./ha3d-native-state-icons.js?v=native-state-icons-1";
 import "./ha3d-custom-view-drawer.js?v=custom-view-drawer-1";
 import "./ha3d-camera-lens.js?v=camera-fisheye-1";
 import "./ha3d-icon-standard.js?v=mdi-standard-2";
+import "./ha3d-easy-floorplan-test.js?v=easy-floorplan-test-1";
