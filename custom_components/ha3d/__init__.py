e95df41a3c61851c5069a99f5de339aa84c143b9
Jackson Gomes <97919205+Jackson-Gomes@users.noreply.github.com>
2026-09-21 00:25:49 -0300

    Version HA3D panel URL for stable filter switching

--- custom_components/ha3d/__init__.py
+++ custom_components/ha3d/__init__.py
@@ -21,7 +21,7 @@ from .http import HA3DConfigView, HA3DModelUploadView
 from .storage import HA3DStore
 
 
-FRONTEND_VERSION = "0.1.18"
+FRONTEND_VERSION = "0.1.19"
 
 
 async def async_setup(hass: HomeAssistant, config: ConfigType) -> bool:
