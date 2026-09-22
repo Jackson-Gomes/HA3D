5f0672e99104c09705fd04a874200ef00bb67f73
Jackson Gomes <97919205+Jackson-Gomes@users.noreply.github.com>
2026-09-21 23:03:11 -0300

    Version HA3D frontend for classic X-Ray restore

--- custom_components/ha3d/__init__.py
+++ custom_components/ha3d/__init__.py
@@ -21,7 +21,7 @@ from .http import HA3DAreasView, HA3DConfigView, HA3DModelUploadView
 from .storage import HA3DStore
 
 
-FRONTEND_VERSION = "0.2.17"
+FRONTEND_VERSION = "0.2.18"
 
 
 async def async_setup(hass: HomeAssistant, config: ConfigType) -> bool:
