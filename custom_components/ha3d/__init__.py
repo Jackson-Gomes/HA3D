c8f349fa98b8bf8e7db5e1e2c77b26862c7607aa
Jackson Gomes <97919205+Jackson-Gomes@users.noreply.github.com>
2026-09-22 16:34:25 -0300

    Bump HA3D frontend cache to 0.2.38

--- custom_components/ha3d/__init__.py
+++ custom_components/ha3d/__init__.py
@@ -21,7 +21,7 @@ from .http import HA3DAreasView, HA3DConfigView, HA3DModelUploadView
 from .storage import HA3DStore
 
 
-FRONTEND_VERSION = "0.2.37"
+FRONTEND_VERSION = "0.2.38"
 
 
 async def async_setup(hass: HomeAssistant, config: ConfigType) -> bool:
