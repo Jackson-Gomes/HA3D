f2d4ecf4e5330b02217cf2b6dbef58a9fc8f1044
Jackson Gomes <97919205+Jackson-Gomes@users.noreply.github.com>
2026-09-22 20:10:58 -0300

    feat: load floating widget layer

--- custom_components/ha3d/frontend/ha3d-entry.js
+++ custom_components/ha3d/frontend/ha3d-entry.js
@@ -45,3 +45,4 @@ import "./ha3d-robot-trackers.js?v=robot-trackers-9";
 import "./ha3d-robot-map-overlay.js?v=robot-map-overlay-5";
 import "./ha3d-robot-map-hotfix.js?v=robot-map-hotfix-1";
 import "./ha3d-robot-map-shared-config.js?v=robot-map-shared-config-1";
+import "./ha3d-floating-widgets.js?v=floating-widgets-1";
