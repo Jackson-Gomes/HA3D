6e4ab6f8be9bbb8eb55b209a0c3f704108abb208
Jackson Gomes <97919205+Jackson-Gomes@users.noreply.github.com>
2026-09-22 10:38:10 -0300

    Load editor drag hotfix

--- custom_components/ha3d/frontend/ha3d-entry.js
+++ custom_components/ha3d/frontend/ha3d-entry.js
@@ -29,6 +29,7 @@ import "./ha3d-custom-view-drawer.js?v=custom-view-drawer-1";
 import "./ha3d-camera-lens.js?v=camera-fisheye-1";
 import "./ha3d-icon-standard.js?v=mdi-standard-2";
 import "./ha3d-easy-floorplan-test.js?v=easy-floorplan-test-3";
+import "./ha3d-editor-drag-hotfix.js?v=editor-drag-hotfix-1";
 import "./ha3d-robot-trackers.js?v=robot-trackers-9";
 import "./ha3d-robot-map-overlay.js?v=robot-map-overlay-4";
 import "./ha3d-robot-map-hotfix.js?v=robot-map-hotfix-1";
