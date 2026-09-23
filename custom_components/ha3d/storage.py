6a27dc18ecb66bdf209d8d3d962e0e99f3626f87
Jackson Gomes <97919205+Jackson-Gomes@users.noreply.github.com>
2026-09-22 21:43:51 -0300

    feat: persist HA3D entity aliases

--- custom_components/ha3d/storage.py
+++ custom_components/ha3d/storage.py
@@ -21,6 +21,7 @@ DEFAULT_CONFIG: dict[str, Any] = {
     "virtual_lights": [],
     "scene_assets": [],
     "floating_widgets": [],
+    "entity_aliases": {},
 }
 
 
@@ -40,6 +41,7 @@ class HA3DStore:
             self._data["virtual_lights"] = list(stored.get("virtual_lights", []))
             self._data["scene_assets"] = list(stored.get("scene_assets", []))
             self._data["floating_widgets"] = list(stored.get("floating_widgets", []))
+            self._data["entity_aliases"] = dict(stored.get("entity_aliases", {}))
         return deepcopy(self._data)
 
     async def async_update(self, changes: dict[str, Any]) -> dict[str, Any]:
