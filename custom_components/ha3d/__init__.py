0307da1be0f34c4b4c56feaeb526e252323c3ba4
Jackson Gomes <97919205+Jackson-Gomes@users.noreply.github.com>
2026-09-23 12:06:02 -0300

    chore: bump HA3D frontend to 0.2.61

--- custom_components/ha3d/__init__.py
+++ custom_components/ha3d/__init__.py
@@ -26,7 +26,7 @@ from .storage import HA3DStore
 from .virtual_lights import HA3DVirtualLightsView
 
 
-FRONTEND_VERSION = "0.2.60"
+FRONTEND_VERSION = "0.2.61"
 
 
 async def async_setup(hass: HomeAssistant, config: ConfigType) -> bool:
