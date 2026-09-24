b1ae7a1082ac40e7e89876e7d2e07ff458f33d3d
Jackson Gomes <97919205+Jackson-Gomes@users.noreply.github.com>
2026-09-24 16:50:19 -0300

    chore(webxr): align frontend token with beta.4

--- custom_components/ha3d/__init__.py
+++ custom_components/ha3d/__init__.py
@@ -26,7 +26,7 @@ from .storage import HA3DStore
 from .virtual_lights import HA3DVirtualLightsView
 
 
-FRONTEND_VERSION = "0.2.65-beta.3"
+FRONTEND_VERSION = "0.2.65-beta.4"
 
 
 async def async_setup(hass: HomeAssistant, config: ConfigType) -> bool:
