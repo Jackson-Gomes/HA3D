313ea878adc57c3e1990277b5da2881a387364da
Jackson Gomes <97919205+Jackson-Gomes@users.noreply.github.com>
2026-09-24 16:11:16 -0300

    chore(webxr): bust frontend cache for Quest test build

--- custom_components/ha3d/__init__.py
+++ custom_components/ha3d/__init__.py
@@ -26,7 +26,7 @@ from .storage import HA3DStore
 from .virtual_lights import HA3DVirtualLightsView
 
 
-FRONTEND_VERSION = "0.2.59"
+FRONTEND_VERSION = "0.2.59-webxr1"
 
 
 async def async_setup(hass: HomeAssistant, config: ConfigType) -> bool:
