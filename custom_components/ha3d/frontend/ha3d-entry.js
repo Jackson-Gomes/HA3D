4bc2d461b938b4acfaceffc93ee62892b6808a23
Jackson Gomes <97919205+Jackson-Gomes@users.noreply.github.com>
2026-09-24 20:05:03 -0300

    Load Quest gamepad AR controls

--- custom_components/ha3d/frontend/ha3d-entry.js
+++ custom_components/ha3d/frontend/ha3d-entry.js
@@ -61,3 +61,4 @@ import "./ha3d-floating-state-fix.js?v=floating-state-fix-1";
 import "./ha3d-bottom-view-hub.js?v=bottom-view-hub-1";
 import "./ha3d-webxr-ar-v3.js?v=ar-v3-1";
 import "./ha3d-webxr-ar-tabletop-fix.js?v=tabletop-fix-3";
+import "./ha3d-webxr-ar-game-controls.js?v=game-controls-1";
