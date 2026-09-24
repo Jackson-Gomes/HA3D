b28be6f1069c2b3cfdc1e5fd2a65ab0d3347d59e
Jackson Gomes <97919205+Jackson-Gomes@users.noreply.github.com>
2026-09-24 20:05:46 -0300

    Set frontend version to 0.2.65-beta.12

--- custom_components/ha3d/__init__.py
+++ custom_components/ha3d/__init__.py
@@ -26,7 +26,7 @@ from .storage import HA3DStore
 from .virtual_lights import HA3DVirtualLightsView
 
 
-FRONTEND_VERSION = "0.2.65-beta.11"
+FRONTEND_VERSION = "0.2.65-beta.12"
 
 
 async def async_setup(hass: HomeAssistant, config: ConfigType) -> bool:
