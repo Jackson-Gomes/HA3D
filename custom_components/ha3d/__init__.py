0cba93b12dbb2cb49500bbecaad2572b5c506e90
Jackson Gomes <97919205+Jackson-Gomes@users.noreply.github.com>
2026-09-24 19:27:14 -0300

    Bump frontend cache version to beta.10

--- custom_components/ha3d/__init__.py
+++ custom_components/ha3d/__init__.py
@@ -26,7 +26,7 @@ from .storage import HA3DStore
 from .virtual_lights import HA3DVirtualLightsView
 
 
-FRONTEND_VERSION = "0.2.65-beta.9"
+FRONTEND_VERSION = "0.2.65-beta.10"
 
 
 async def async_setup(hass: HomeAssistant, config: ConfigType) -> bool:
